fbd6a855be8237edde65db12be5e200adae8e04c
Eryllaaa <allyre@gmail.com>
2024-05-28 00:17:22 +0200

    feat: RecipeStruct.Equals() and Glass.GetRecipeFromContents()

--- Scripts/RecipeManager/RecipeStruct.cs
+++ Scripts/RecipeManager/RecipeStruct.cs
@@ -1,12 +1,27 @@
 using Scripts.RecipeScripts.Condiments;
 using Scripts.RecipeScripts.Liquids;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
 
 namespace Scripts.RecipeScripts {
     public struct RecipeStruct {
-        public float shakeValue;
-        public List<LiquidEnum> liquids;
-        public List<CondimentEnum> condiments;
+        public float _shakeValue;
+        public List<LiquidEnum> _liquids;
+        public List<CondimentEnum> _condiments;
+
+        public RecipeStruct(float shakeValue, List<LiquidEnum> liquids, List<CondimentEnum> condiment) {
+            _shakeValue = shakeValue;
+            _liquids = new List<LiquidEnum>(liquids);
+            _condiments = new List<CondimentEnum>(condiment);
+        }
+
+        public bool Equals(RecipeStruct other, float difficultyMargin = 0) {
+            bool sameShakeValue = other._shakeValue < _shakeValue + difficultyMargin && other._shakeValue > _shakeValue - difficultyMargin;
+            bool sameLiquids = !_liquids.Except(other._liquids).Any();
+            bool sameCondiments = !_condiments.Except(other._condiments).Any();
+            return sameShakeValue && sameLiquids && sameCondiments;
+        }
     }
 }
 
--- Scripts/UI/StandElements/Movables/Glass.cs
+++ Scripts/UI/StandElements/Movables/Glass.cs
@@ -4,12 +4,18 @@ using Scripts.RecipeScripts.Liquids;
 using Scripts.RecipeScripts.Condiments;
 using System.Collections.Generic;
 using System;
+using Scripts.RecipeScripts;
 
 namespace Scripts.UI.StandElements.Movables {
     public partial class Glass : Movable {
+        public float shakeValue;
         public List<LiquidEnum> liquids;
         public List<CondimentEnum> condiments;
         private iGlassState stateGlass;
+
+        public RecipeStruct GetRecipeFromContents() {
+            return new RecipeStruct(shakeValue, liquids, condiments);
+        }
     }
 }
 
