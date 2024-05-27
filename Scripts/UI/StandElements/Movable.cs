5edafedfa3744890666239f595d079db86638a7f
Eryllaaa <allyre@gmail.com>
2024-05-27 17:06:59 +0200

    feat: movable state machine and moavblebody leftclick detection

--- Scripts/UI/StandElements/Movable.cs
+++ Scripts/UI/StandElements/Movable.cs
@@ -1,21 +1,43 @@
 using Godot;
 
 namespace Scripts.UI.StandElements {
-    public abstract partial class Movable : Node2D {
+    public /*abstract*/ partial class Movable : Node2D {
 
         [Export] private NodePath bodyPath;
         private MovableBody body;
+
+        private Vector2 mouseOffset;
+
+        public delegate void State(double delta);
+        public State state;
         
         public override void _Ready() {
             body = GetNode<MovableBody>(bodyPath);
+            body.movableParent = this;
+
+            state = DroppedState;
         }
 
         public override void _Process(double delta) {
+            state(delta);
         }
 
-        public virtual void PickUp() {
+        public virtual void PickedUpState(double delta) {
+            Position = mouseOffset + GetGlobalMousePosition();
+
         }
-        public virtual void Drop() {
+
+        public virtual void DroppedState(double delta) {
+
+        }
+
+        public virtual void SetPickedUp() {
+            state = PickedUpState;
+            mouseOffset = Position - GetGlobalMousePosition();
+        }
+
+        public virtual void SetDropped() {
+            state = DroppedState;
         }
     }
 }
--- Scripts/UI/StandElements/Movables/MovableBody.cs
+++ Scripts/UI/StandElements/Movables/MovableBody.cs
@@ -4,10 +4,19 @@ using System;
 namespace Scripts.UI.StandElements {
 	public partial class MovableBody : Area2D {
 		
-		public override void _Ready() {
-		}
+        public Movable movableParent;
 
-		public override void _Process(double delta) {
-		}
-	}
+        public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx) {
+            //left click pressed
+            if (@event is InputEventMouseButton && ((InputEventMouseButton)@event).ButtonIndex == MouseButton.Left && ((InputEventMouseButton)@event).Pressed) {
+                movableParent.SetPickedUp();
+            }
+            //left click released
+            else if (@event is InputEventMouseButton && ((InputEventMouseButton)@event).ButtonIndex == MouseButton.Left && !((InputEventMouseButton)@event).Pressed) {
+                if (movableParent.state == movableParent.PickedUpState) {
+                    movableParent.SetDropped();
+                }
+            }
+        }
+    }
 }
