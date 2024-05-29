39074b5eb2f18ad71dc3eabbef015d95d71ff5e0
Eryllaaa <allyre@gmail.com>
2024-05-29 19:02:23 +0200

    feat: added StandInfo struct

--- Scripts/UI/Stand/Stand.cs
+++ Scripts/UI/Stand/Stand.cs
@@ -1,8 +1,56 @@
 ﻿using Godot;
+using System;
 
 namespace Scripts.UI.StandScripts {
-    [GlobalClass]
+    public struct StandInfo {
+
+        public int lastWaveSinceHitOnStand;
+        public float playerZombKillEfficiency;
+        public float playerBarmanEfficiency;
+        public int numCustomersServed;
+        public int standHealth;
+        public int numAltDrinksPrepared;
+
+        public StandInfo(int lastWaveSinceHitOnStand, float playerZombKillEfficiency, float playerBarmanEfficiency, int numCustomersServed, int standHealth, int numAltDrinksPrepared) {
+            this.lastWaveSinceHitOnStand = lastWaveSinceHitOnStand;
+            this.playerZombKillEfficiency = playerZombKillEfficiency;
+            this.playerBarmanEfficiency = playerBarmanEfficiency;
+            this.numCustomersServed = numCustomersServed;
+            this.standHealth = standHealth;
+            this.numAltDrinksPrepared = numAltDrinksPrepared;
+        }
+    }
+
     public partial class Stand : Node2D {
+
+        [Export] private int startHealth;
+        private int health;
+        private int money = 0;
         public Area2D standArea;
+
+        private int lastWaveSinceHitOnStand;
+        private float playerZombKillEfficiency;
+        private float playerBarmanEfficiency;
+        private int numCustomersServed;
+        private int standHealth;
+        private int numAltDrinksPrepared;
+
+        private Action<StandInfo> GetStandInfo;
+
+        public override void _Ready() {
+            health = startHealth;
+        }
+
+        public void Start() {
+            GetStandInfo(GetStandInfoStruct());
+        }
+
+        public void BindGetStandInfo(Action<StandInfo> action) { 
+            GetStandInfo += action;
+        }
+
+        private StandInfo GetStandInfoStruct() {
+            return new StandInfo(lastWaveSinceHitOnStand, playerZombKillEfficiency, playerBarmanEfficiency, numCustomersServed, standHealth, numAltDrinksPrepared);
+        }
     }
 }
