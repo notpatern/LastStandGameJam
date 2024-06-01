c64b00952ae1db6a7b0542946e91c14d69fbbe89
choufchouf123 <68738351+choufchouf123@users.noreply.github.com>
2024-06-01 23:29:51 +0300

    Glass Dispenser
    finishing the glass dispenser class

--- Scripts/UI/StandElements/GlassDispenser.cs
+++ Scripts/UI/StandElements/GlassDispenser.cs
@@ -1,15 +1,30 @@
 using Godot;
 using System;
 
-public partial class GlassDispenser : Node
+public partial class GlassDispenser : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export] PackedScene glassFactory;
+    [Export] Node2D spawnPoint;
+    [Export] float distanceBeforeSpawnGlass;
+    Node2D lastDispensedGlass;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready() {
+        lastDispensedGlass = SpawnObject(glassFactory, spawnPoint.GlobalPosition);
+    }
+    public override void _Process(double delta) {
+        if (lastDispensedGlass != null) {
+            if (lastDispensedGlass.GlobalPosition.DistanceTo(spawnPoint.GlobalPosition) > distanceBeforeSpawnGlass)
+                lastDispensedGlass = SpawnObject(glassFactory, spawnPoint.GlobalPosition);
+        }
+    }
+    private Node2D SpawnObject(PackedScene pSpawnable, Vector2 pPos) {
+
+        Node2D lSpawnedObject;
+
+        lSpawnedObject = (Node2D)pSpawnable.Instantiate();
+        lSpawnedObject.AddChild(this);
+        lSpawnedObject.GlobalPosition = pPos;
+
+        return lSpawnedObject;
+    }
 }
