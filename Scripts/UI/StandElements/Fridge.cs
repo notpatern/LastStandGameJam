adf953f784da33d28f81811f9f4408a21503af32
choufchouf123 <68738351+choufchouf123@users.noreply.github.com>
2024-06-01 19:23:12 +0300

    Fridge Done
    finishing the fridge script

--- Scripts/UI/StandElements/Fridge.cs
+++ Scripts/UI/StandElements/Fridge.cs
@@ -1,6 +1,5 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 
 namespace Scripts.UI.StandElements {
@@ -14,7 +13,22 @@ namespace Scripts.UI.StandElements {
 
         public override void _Process(double delta) {
             if (Input.IsActionJustPressed(spawnInputName)) {
+                foreach (CollisionShape2D lFridgeZone in fridgeZones) {
 
+                    if (lFridgeZone.Shape.GetRect().HasPoint(GetGlobalMousePosition()))
+                        SpawnObject(elementsInFridge[Array.IndexOf(fridgeZones, lFridgeZone)], GetGlobalMousePosition());
+                }
+            }
+        }
+        private void SpawnObject(PackedScene pSpawnable, Vector2 pPos, int pNumber = 1) {
+
+            Node2D lSpawnedObject;
+
+            for (int i = 0; i < pNumber; i++) {
+
+                lSpawnedObject = (Node2D)pSpawnable.Instantiate();
+                lSpawnedObject.AddChild(this);
+                lSpawnedObject.GlobalPosition = pPos;
             }
         }
     }
